5f8fab9f1805c34f4d1b7e8de3a1158b8c9690d8
thomasvn <thomasnguyen96@gmail.com>
2017-10-19 18:19:43 -0700

    Refining the definition of the tables

--- createdb.sql
+++ createdb.sql
@@ -1,5 +1,5 @@
 ----------------------------------- Users ------------------------------------
-CREATE TABLE YelpUser {
+CREATE TABLE YelpUser (
     YelpUserID INTEGER,
     FirstName VARCHAR(256),
     LastName VARCHAR(256),
@@ -7,36 +7,38 @@ CREATE TABLE YelpUser {
     BirthPlace VARCHAR(256),
     EmailID INTEGER,
     Gender VARCHAR(256),
+    ProfilePicture VARBINARY(MAX),
 
     PRIMARY KEY (YelpUserID),
-};
+);
 
-CREATE TABLE YelpFriend {
-    YelpFriendID INTEGER,
+CREATE TABLE YelpFriend (
+    YelpFriendID INTEGER NOT NULL,
     YelpUserID INTEGER,
     Compliment VARCHAR(256), -- Can be NULL
-    ViewRecommendedReviews INTEGER, -- Acts as a boolean
-    VotedReview INTEGER, -- Acts as a boolean
-    PhotoIDMarkedHelpful INTEGER, -- Can be NULL
-};
+    CanViewRecommendedReviews INTEGER, -- Acts as a boolean
+    VotedOnReview INTEGER, -- Acts as a boolean. Only votes are funny, cool, helpful
+    PhotoIDMarkedHelpful INTEGER, -- Acts as a boolean
+
+    -- Can this Foreign Key stand by itself??
+    FOREIGN KEY (YelpUserID) REFERENCES YelpUser(YelpUserID)
+);
 
 
 ---------------------------------- Businesses ----------------------------------
-CREATE TABLE Business {
+CREATE TABLE Business (
     BusinessID INTEGER,
     Name VARCHAR(256),
     PhoneNum VARCHAR(256),
-    Address VARCHAR(256),
-    ReviewCount VARCHAR(256),
+    Address VARCHAR(256), -- Split up this address??
+    ReviewCount INTEGER,
     BusinessCategoryID INTEGER,
-    RecommendedReviewID INTEGER,
 
     PRIMARY KEY (BusinessID),
-    FOREIGN KEY (BusinessCategoryID) REFERENCES BusinessCategory,
-    FOREIGN KEY (RecommendedReviewID) REFERENCES RecommendedReview,
-};
+    FOREIGN KEY (BusinessCategoryID) REFERENCES BusinessCategory(BusinessCategoryID),
+);
 
-CREATE TABLE Photo {
+CREATE TABLE Photo (
     PhotoID INTEGER,
     PhotoDescription VARCHAR (256),
     Location VARCHAR (256),
@@ -44,66 +46,54 @@ CREATE TABLE Photo {
     YelpUserID INTEGER,
 
     PRIMARY KEY (PhotoID),
-    FOREIGN KEY (BusinessID) REFERENCES Business,
-    FOREIGN KEY (YelpUserID) REFERENCES YelpUser
-}
+    FOREIGN KEY (BusinessID) REFERENCES Business(BusinessID),
+    FOREIGN KEY (YelpUserID) REFERENCES YelpUser(YelpUserID), -- don't we need to make sure that the Photo is tied back to the user??
+)
 
-CREATE TABLE BusinessCategory {
+CREATE TABLE BusinessCategory (
     BusinessCategoryID INTEGER,
     Name VARCHAR(256),
-    -- How do I inherit all the different types of businesses?
-};
+    Type VARCHAR(256), -- Does this have to be NOT NULL??
+    BusinessID INTEGER,
+
+    PRIMARY KEY (BusinessCategoryID),
+    FOREIGN KEY (BusinessID) REFERENCES Business(BusinessID),
+);
 
 
 ----------------------------------- Reviews ------------------------------------
-CREATE TABLE RecommendedTextReview {
+CREATE TABLE Review (
     ReviewID INTEGER,
     Rating INTEGER,
     PublishDate DATE,
+    IsRecommended INTEGER, -- Acts as a boolean
     YelpUserID INTEGER,
     BusinessID INTEGER,
-    Content VARCHAR(256),
 
     PRIMARY KEY (ReviewID),
-    FOREIGN KEY (YelpUserID) REFERENCES YelpUser,
-    FOREIGN KEY (BusinessID) REFERENCES Business,
-}
+    FOREIGN KEY (YelpUserID) REFERENCES YelpUser(YelpUserID),
+    FOREIGN KEY (BusinessID) REFERENCES Business(BusinessID),
+);
 
-CREATE TABLE NotRecommendedTextReview {
+-- for ReviewContent ... do we really need to identify the type??
+-- Also ... Joey has two ReviewIDs
+CREATE TABLE ReviewContent (
+    ReviewContentID INTEGER,
+    ContentText VARCHAR(256), -- Will be NULL if content is photo
+    ContentPhoto VARBINARY(MAX), -- Will be NULL if content is text
     ReviewID INTEGER,
-    Rating INTEGER,
-    PublishDate DATE,
+
+    PRIMARY KEY (ReviewContentID),
+    FOREIGN KEY (ReviewID) REFERENCES Review(ReviewID),
+);
+
+CREATE TABLE MarkedReview (
+    IsRecommendedReview INTEGER, -- Acts as a boolean
     YelpUserID INTEGER,
-    BusinessID INTEGER,
-    Content VARCHAR(256),
-
-    PRIMARY KEY (ReviewID),
-    FOREIGN KEY (YelpUserID) REFERENCES YelpUser,
-    FOREIGN KEY (BusinessID) REFERENCES Business,
-}
-
-CREATE TABLE RecommendedPhotoReview {
     ReviewID INTEGER,
-    Rating INTEGER,
-    PublishDate DATE,
-    YelpUserID INTEGER,
-    BusinessID INTEGER,
-    Content VARBINARY(MAX),
 
-    PRIMARY KEY (ReviewID),
-    FOREIGN KEY (YelpUserID) REFERENCES YelpUser,
-    FOREIGN KEY (BusinessID) REFERENCES Business,
-}
+    FOREIGN KEY (YelpUserID) REFERENCES YelpUser(YelpUserID),
+    FOREIGN KEY (ReviewID) REFERENCES Review(ReviewID),
+);
 
-CREATE TABLE NotRecommendedPhotoReview {
-    ReviewID INTEGER,
-    Rating INTEGER,
-    PublishDate DATE,
-    YelpUserID INTEGER,
-    BusinessID INTEGER,
-    Content VARBINARY(MAX),
-
-    PRIMARY KEY (ReviewID),
-    FOREIGN KEY (YelpUserID) REFERENCES YelpUser,
-    FOREIGN KEY (BusinessID) REFERENCES Business,
-}
+----------------------------------- Queries ------------------------------------
